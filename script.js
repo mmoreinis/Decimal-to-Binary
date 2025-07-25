caff77b0f5e33da424d084fd2d5eb5e7cb5376d3
Martin Bram Moreinis <martin.moreinis@schoolsofwestfield.org>
2025-07-25 13:37:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-var decimal = prompt("Enter a decimal to convert or 0 to end.");
-if (decimal == 0) alert("Thanks for converting!");
-else {  
-  var binary = [];
-  var quotient = decimal; 
-  var remainder = 0;
-  while (quotient > 0) {
-    remainder = quotient % 2;
-    binary.push(remainder);
-    quotient = (quotient-remainder) / 2;
+function convert(){
+  let decimal = prompt("Enter a decimal to convert or 0 to end.");
+  if (decimal == 0) alert("Thanks for converting!");
+  else {  
+    let binary = [];
+    let quotient = decimal; 
+    let remainder = 0;
+    while (quotient > 0) {
+      remainder = quotient % 2;
+      binary.push(remainder);
+      quotient = (quotient-remainder) / 2;
+    }
+    let converted = binary.reverse();
+    alert(converted.join(""));
   }
-  var converted = binary.reverse();
-  alert(converted.join(""));
 }
